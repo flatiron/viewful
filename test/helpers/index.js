f28ad809af3cb6ea197a4ace0dc8eb657fac5b04
Eric Schmitt <ericschmitt1@gmail.com>
2012-07-30 21:47:42 -0700

    [fix] Fix bug inputEngine.render() method, needed self.template passed as first param

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -4,8 +4,8 @@ var assert = require('assert');
 helpers.render = function (data, expected) {
   expected = expected || '';
   return {
-    topic: function(_view){
-      this.callback(null, _view.render(data));
+    topic: function (_view) {
+      _view.render(data, this.callback);
     },
     'should compile expected result' : function (err, result) {
       assert.equal(result, expected);
