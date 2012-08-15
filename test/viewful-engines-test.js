c980f2102f27f43e9450095a4ba1e889ef92d139
Eric Schmitt <ericschmitt1@gmail.com>
2012-08-15 02:53:46 -0700

    [jazz] [test] Add jazz engine plugin and associated test for async render

--- test/viewful-engines-test.js
+++ test/viewful-engines-test.js
@@ -70,6 +70,14 @@ vows.describe('viewful-engines-test').addBatch({
       }),
       'and calling View.render(user)': helpers.renderSync(user, "<p>tobi</p>"),
       'and calling View.render(user, cb)': helpers.render(user, "<p>tobi</p>")
+    },
+
+    'a new viewful.View({ input: "jazz" })': {
+      topic: new viewful.View({
+        template: "<p>{user.name}</p>",
+        input: "jazz"
+      }),
+      'and calling View.render(user, cb)': helpers.render(user, "<p>tobi</p>")
     }
 
   }
