59bd631cea0a8ee1c0a876e5c167541f2b75124f
Eric Schmitt <ericschmitt1@gmail.com>
2012-08-16 16:30:29 -0700

    [haml] [test] Add haml engine plugin and associated tests for sync and async render

--- test/viewful-engines-test.js
+++ test/viewful-engines-test.js
@@ -87,6 +87,15 @@ vows.describe('viewful-engines-test').addBatch({
       }),
       'and calling View.render(user)': helpers.renderSync(user, "<p>tobi</p>"),
       'and calling View.render(user, cb)': helpers.render(user, "<p>tobi</p>")
+    },
+
+    'a new viewful.View({ input: "haml" })': {
+      topic: new viewful.View({
+        template: "%p= user.name",
+        input: "haml"
+      }),
+      'and calling View.render(user)': helpers.renderSync(user, "<p>tobi</p>"),
+      'and calling View.render(user, cb)': helpers.render(user, "<p>tobi</p>")
     }
 
   }
