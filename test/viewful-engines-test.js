fed10a0297c46af540e2048e9ace01acab87f152
Eric Schmitt <ericschmitt1@gmail.com>
2012-08-17 03:47:34 -0700

    [mustache] [test] [dist] Add mustache engine plugin and associated tests for sync and async render(), Add mustache template engine to devDependencies

--- test/viewful-engines-test.js
+++ test/viewful-engines-test.js
@@ -158,6 +158,15 @@ vows.describe('viewful-engines-test').addBatch({
       }),
       'and calling View.render(user)': helpers.renderSync(user, "<p>tobi</p>"),
       'and calling View.render(user, cb)': helpers.render(user, "<p>tobi</p>")
+    },
+
+    'a new viewful.View({ input; "mustache" })': {
+      topic: new viewful.View({
+        template: "<p>{{user.name}}</p>",
+        input: "mustache"
+      }),
+      'and calling View.render(user)': helpers.renderSync(user, "<p>tobi</p>"),
+      'and calling View.render(user, cb)': helpers.render(user, "<p>tobi</p>")
     }
 
   }
