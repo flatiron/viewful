3bb79b0d8db8fc573fa84e155cbcbabd743a07a2
Eric Schmitt <ericschmitt1@gmail.com>
2012-08-17 03:17:15 -0700

    [walrus] [test] [dist] Add walrus engine plugin and associated tests for sync and async render(), Add walrus template engine to devDependencies

--- test/viewful-engines-test.js
+++ test/viewful-engines-test.js
@@ -149,6 +149,15 @@ vows.describe('viewful-engines-test').addBatch({
         input: "qejs"
       }),
       'and calling View.render(user, cb)': helpers.render(user, "<p>tobi</p>")
+    },
+
+    'a new viewful.View({ input: "walrus" })': {
+      topic: new viewful.View({
+        template: "<p>{{user.name}}</p>",
+        input: "walrus"
+      }),
+      'and calling View.render(user)': helpers.renderSync(user, "<p>tobi</p>"),
+      'and calling View.render(user, cb)': helpers.render(user, "<p>tobi</p>")
     }
 
   }
