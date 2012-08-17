73eeec352f81f719127066825dc469c58cc6a1d8
Eric Schmitt <ericschmitt1@gmail.com>
2012-08-17 02:44:46 -0700

    [qejs] [test] [dist] Add qejs engine plugin and associated test for async render(), Add template engine to devDependencies

--- test/viewful-engines-test.js
+++ test/viewful-engines-test.js
@@ -141,6 +141,14 @@ vows.describe('viewful-engines-test').addBatch({
       }),
       'and calling View.render(user)': helpers.renderSync(user, "<p>tobi</p>"),
       'and calling View.render(user, cb)': helpers.render(user, "<p>tobi</p>")
+    },
+
+    'a new viewful.View({ input: "qejs" })': {
+      topic: new viewful.View({
+        template: "<p><%= user.name %></p>",
+        input: "qejs"
+      }),
+      'and calling View.render(user, cb)': helpers.render(user, "<p>tobi</p>")
     }
 
   }
