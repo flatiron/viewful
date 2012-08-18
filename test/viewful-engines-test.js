82b2a1d08f10d9da03bdcaec46ebfa5c99fb4648
Eric Schmitt <ericschmitt1@gmail.com>
2012-08-18 00:46:34 -0700

    [liquor] [test] [refactor] Move require statement to init(), Invoke init() in test

--- test/viewful-engines-test.js
+++ test/viewful-engines-test.js
@@ -44,10 +44,13 @@ vows.describe('viewful-engines-test').addBatch({
     },
 
     'a new viewful.View({ input: "liquor" })': {
-      topic: new viewful.View({
-        template: "<p>#{user.name}</p>",
-        input: "liquor"
-      }),
+      topic: function () {
+        viewful.engines.init();
+        return new viewful.View({
+          template: "<p>#{user.name}</p>",
+          input: "liquor"
+        })
+      },
       'and calling View.render(user)': helpers.renderSync(user, "<p>tobi</p>"),
       'and calling View.render(user, cb)': helpers.render(user, "<p>tobi</p>")
     },
