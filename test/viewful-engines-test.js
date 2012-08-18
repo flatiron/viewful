428519387223e2c12871c564e1ef2d903c769416
Eric Schmitt <ericschmitt1@gmail.com>
2012-08-18 01:54:53 -0700

    [whiskers] [test] [refactor] Move require to init(), Invoke init() in test

--- test/viewful-engines-test.js
+++ test/viewful-engines-test.js
@@ -126,10 +126,13 @@ vows.describe('viewful-engines-test').addBatch({
     },
 
     'a new viewful.View({ input: "whiskers" })': {
-      topic: new viewful.View({
-        template: "<p>{user.name}</p>",
-        input: "whiskers"
-      }),
+      topic: function () {
+        viewful.engines.init();
+        return new viewful.View({
+          template: "<p>{user.name}</p>",
+          input: "whiskers"
+        });
+      },
       'and calling View.render(user)': helpers.renderSync(user, "<p>tobi</p>"),
       'and calling View.render(user, cb)': helpers.render(user, "<p>tobi</p>")
     },
