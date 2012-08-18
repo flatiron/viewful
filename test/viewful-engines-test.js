d24894a71d4f2e85da97005995068cadbc40fb8c
Eric Schmitt <ericschmitt1@gmail.com>
2012-08-18 01:48:48 -0700

    [jqtpl] [test] [refactor] Move require to init(), Invoke init() in test

--- test/viewful-engines-test.js
+++ test/viewful-engines-test.js
@@ -102,10 +102,13 @@ vows.describe('viewful-engines-test').addBatch({
     },
 
     'a new viewful.View({ input: "jqtpl" })': {
-      topic: new viewful.View({
-        template: "<p>${user.name}</p>",
-        input: "jqtpl"
-      }),
+      topic: function () {
+        viewful.engines.init();
+        return new viewful.View({
+          template: "<p>${user.name}</p>",
+          input: "jqtpl"
+        });
+      },
       'and calling View.render(user)': helpers.renderSync(user, "<p>tobi</p>"),
       'and calling View.render(user, cb)': helpers.render(user, "<p>tobi</p>")
     },
