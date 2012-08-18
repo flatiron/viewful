d767802f5c224c0c4c42aa8736b2021c955a36cc
Eric Schmitt <ericschmitt1@gmail.com>
2012-08-18 01:51:32 -0700

    [haml] [test] [refactor] Move require to init(), Invoke init() in test

--- test/viewful-engines-test.js
+++ test/viewful-engines-test.js
@@ -114,10 +114,13 @@ vows.describe('viewful-engines-test').addBatch({
     },
 
     'a new viewful.View({ input: "haml" })': {
-      topic: new viewful.View({
-        template: "%p= user.name",
-        input: "haml"
-      }),
+      topic: function () {
+        viewful.engines.init();
+        return new viewful.View({
+          template: "%p= user.name",
+          input: "haml"
+        });
+      },
       'and calling View.render(user)': helpers.renderSync(user, "<p>tobi</p>"),
       'and calling View.render(user, cb)': helpers.render(user, "<p>tobi</p>")
     },
