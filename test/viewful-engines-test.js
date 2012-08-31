144acfc61588b422fdcee51781e9a9bdfbdb01ef
Eric Schmitt <ericschmitt1@gmail.com>
2012-08-30 23:14:58 -0700

    [test] [refactor] Cleanup, alphabetize engine keys

--- test/viewful-engines-test.js
+++ test/viewful-engines-test.js
@@ -7,25 +7,25 @@ var assert = require('assert')
 var user = { user : { name: "tobi" } }
   , expected = '<p>tobi</p>'
   , engines = {
-      'jade': { template: 'p= user.name', syncRender: true }
-    , 'swig': { template: '<p>{{user.name}}</p>', syncRender: true }
-    , 'liquor': { template: '<p>#{user.name}</p>', syncRender: true }
+      'dot': { template: '<p>{{= it.user.name }}</p>', syncRender: true }
     , 'dust': { template: '<p>{#user}{name}{/user}</p>', syncRender: false }
-    , 'ejs': { template: '<p><%= user.name %></p>', syncRender: true }
     , 'eco': { template: '<p><%= @user.name %></p>', syncRender: true }
+    , 'ejs': { template: '<p><%= user.name %></p>', syncRender: true }
+    , 'haml': { template: '%p= user.name', syncRender: true }
+    , 'haml-coffee': { template: '%p= @user.name', syncRender: true }
+    , 'handlebars': { template: '<p>{{user.name}}</p>', syncRender: true }
+    , 'hogan': { template: '<p>{{user.name}}</p>', syncRender: true }
+    , 'jade': { template: 'p= user.name', syncRender: true }
     , 'jazz': { template: '<p>{user.name}</p>', syncRender: false }
     , 'jqtpl': { template: '<p>${user.name}</p>', syncRender: true }
-    , 'haml': { template: '%p= user.name', syncRender: true }
-    , 'whiskers': { template: '<p>{user.name}</p>', syncRender: true }
-    , 'haml-coffee': { template: '%p= @user.name', syncRender: true }
-    , 'hogan': { template: '<p>{{user.name}}</p>', syncRender: true }
-    , 'handlebars': { template: '<p>{{user.name}}</p>', syncRender: true }
-    , 'underscore': { template: '<p><%= user.name %></p>', syncRender: true }
-    , 'qejs': { template: '<p><%= user.name %></p>', syncRender: false }
-    , 'walrus': { template: '<p>{{user.name}}</p>', syncRender: true }
-    , 'mustache': { template: '<p>{{user.name}}</p>', syncRender: true }
-    , 'dot': { template: '<p>{{= it.user.name }}</p>', syncRender: true }
     , 'just': { template: '<p><%= user.name %></p>', syncRender: false }
+    , 'liquor': { template: '<p>#{user.name}</p>', syncRender: true }
+    , 'mustache': { template: '<p>{{user.name}}</p>', syncRender: true }
+    , 'qejs': { template: '<p><%= user.name %></p>', syncRender: false }
+    , 'swig': { template: '<p>{{user.name}}</p>', syncRender: true }
+    , 'underscore': { template: '<p><%= user.name %></p>', syncRender: true }
+    , 'walrus': { template: '<p>{{user.name}}</p>', syncRender: true }
+    , 'whiskers': { template: '<p>{user.name}</p>', syncRender: true }
     }
   ;
 
