d25b8206464e60a6dde35a1997b1b8b8068e510c
Eric Schmitt <ericschmitt1@gmail.com>
2012-09-09 13:33:25 -0400

    [test] Add commented-out unit test of sync present() method for review

--- test/viewful-present-test.js
+++ test/viewful-present-test.js
@@ -1,6 +1,10 @@
 var assert = require('assert')
   , vows = require('vows')
   , viewful = require('../lib/viewful')
+  // TODO: Remove or refactor expected variable after Marak's review. The
+  // "expected" variable is used for testing the present() method, see 
+  // additional TODO note below for more info on testing of present() method.
+  //, expected = '<div><button id="thebutton">Show Alert</button></div>'
   ;
 
 vows.describe('viewful/viewful-present-test').addBatch({
@@ -21,5 +25,22 @@ vows.describe('viewful/viewful-present-test').addBatch({
         assert.isFunction(view.inputs.button.present);
       }
     }
+    /* TODO: Refactor or remove after Marak's review. Not sure how to test
+     * the present() method. I think it needs to be tested client-side, using
+     * a headless browser testing environment like zombie.js, or equivalent..
+     *
+    , 'when loaded and rendered synchronously': {
+      topic: function (view) {
+        var results;
+        view.load();
+        view.render({ label: 'Show Alert' });
+        results = view.button.present();
+        return results;
+      }
+      , 'should return expected results when calling present()': function (results) {
+        assert.equal(results, expected);
+      }
+    }
+    */
   }
 }).export(module);
