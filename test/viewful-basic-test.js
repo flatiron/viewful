16dfa47e3be5031b56afbda121fb813899cab600
Eric Schmitt <ericschmitt1@gmail.com>
2012-09-10 17:20:06 -0400

    [test] [refactor] [minor] Add test and implement default value for input option

--- test/viewful-basic-test.js
+++ test/viewful-basic-test.js
@@ -34,11 +34,9 @@ vows.describe('viewful/viewful-basic-test').addBatch({
       'should contain "render" function': function (_view) {
         assert.isFunction(_view.render);
       },
-      /*
       'should contain default "input"': function (_view) {
-        assert.equal(_view.input, "plates");
+        assert.equal(_view.input, 'html');
       },
-      */
       'should contain default "output"': function (_view) {
         assert.equal(_view.output, "html");
       }
