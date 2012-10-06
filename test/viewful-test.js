ea13655b798bd02a0fb01fe034ea84d8542a83d6
Eric Schmitt <ericschmitt1@gmail.com>
2012-10-05 22:44:33 -0400

    [test] [refactor] Simplify test batch name and remove outer context

--- test/viewful-test.js
+++ test/viewful-test.js
@@ -4,40 +4,39 @@ var assert = require('assert'),
     viewful = require('../lib/viewful');
 
 vows.describe('viewful-test').addBatch({
-  'When using `viewful`': {
-    'the viewful api': {
-      topic: viewful,
-      'should require without error': function (result) {
-        assert.ok(result);
-      },
-      'should contain a top-level View class': function (_viewful) {
-        assert.isFunction(_viewful.View);
-      },
-      'should contain engines': function (_viewful) {
-        assert.isObject(_viewful.engines);
-      },
-      'should have loaded plates engine': function (_viewful) {
-        assert.isObject(_viewful.engines['plates']);
-        assert.isFunction(_viewful.engines['plates'].render);
-      },
-      'should be able to create a new View instance': function (_viewful) {
-        assert.isObject(_viewful.factory());
-      }
+  'viewful': {
+    topic: viewful,
+    'should require without error': function (result) {
+      assert.ok(result);
     },
-    'a new viewful.View() with default options': {
-      topic: viewful.factory(),
-      'should return a new View': function (_view) {
-        assert.isObject(_view);
-      },
-      'should contain "render" function': function (_view) {
-        assert.isFunction(_view.render);
-      },
-      'should contain default "input"': function (_view) {
-        assert.equal(_view.input, 'html');
-      },
-      'should contain default "output"': function (_view) {
-        assert.equal(_view.output, "html");
-      }
+    'should contain a top-level View class': function (_viewful) {
+      assert.isFunction(_viewful.View);
+    },
+    'should contain engines': function (_viewful) {
+      assert.isObject(_viewful.engines);
+    },
+    'should have loaded plates engine': function (_viewful) {
+      assert.isObject(_viewful.engines['plates']);
+      assert.isFunction(_viewful.engines['plates'].render);
+    },
+    'should be able to create a new View instance': function (_viewful) {
+      assert.isObject(_viewful.factory());
+    }
+  },
+
+  'a new viewful.View() with default options': {
+    topic: viewful.factory(),
+    'should return a new View': function (_view) {
+      assert.isObject(_view);
+    },
+    'should contain "render" function': function (_view) {
+      assert.isFunction(_view.render);
+    },
+    'should contain default "input"': function (_view) {
+      assert.equal(_view.input, 'html');
+    },
+    'should contain default "output"': function (_view) {
+      assert.equal(_view.output, "html");
     }
   }
 }).export(module);
